59165b47c10785f6f301f706b4391abd921c44c5
AnkushYadav <yadavankush1811@gmail.com>
2025-08-26 20:50:03 +0530

    router created...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import Navbar from "./components/Navbar.js";
+import { BrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = BrowserRouter([
+    {
+      path:"/",
+      element : <Home/>
+    },
+    {
+      path:"/about",
+      element : <About/>
+    },
+    {
+      path : "/contact",
+      element : <Contact/>
+    }
+  ]);
+
   return (
   <>
-      <Navbar/>
+    <RouterProvider router={router}/>
+    <Navbar/>
   </>
   );
 }
